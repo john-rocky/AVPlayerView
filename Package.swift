893ad355a61c568edcc233cdf5e4ea01566e99bf
john-rocky <rockyshikoku@gmail.com>
2024-01-30 04:28:06 +0900

    ios taget

--- Package.swift
+++ Package.swift
@@ -4,23 +4,23 @@
 import PackageDescription
 
 let package = Package(
-    name: "avplayerview",
+    name: "AVPlayerView",
     platforms: [
         .iOS(.v13) 
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "avplayerview",
-            targets: ["avplayerview"]),
+            name: "AVPlayerView",
+            targets: ["AVPlayerView"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "avplayerview"),
+            name: "AVPlayerView"),
         .testTarget(
-            name: "avplayerviewTests",
-            dependencies: ["avplayerview"]),
+            name: "AVPlayerViewTests",
+            dependencies: ["AVPlayerView"]),
     ]
 )
